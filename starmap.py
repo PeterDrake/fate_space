f7f97236a1eb355e023aeaddef644fd4c0cf801d
Peter Drake <drake@lclark.edu>
2022-04-10 21:21:22 -0700

    Generates tech levels.

--- starmap.py
+++ starmap.py
@@ -2,22 +2,42 @@ import matplotlib.pyplot as plt
 from utils import *
 import pandas as pd
 
+TECH_LEVELS = ('-4 Stone age',
+               '-3 Iron age',
+               '-2 Medieval',
+               '-1 Industrial',
+               '0 Orbital',
+               '+1 Sublight',
+               '+2 Primitive whipdrive',
+               '+3 Advanced whipdrive',
+               '+4 Nigh magical')
+
+ENVIRONMENTS = ('Asteroid',
+                'Vacuum',
+                'Volcano',
+                'Ice',
+                'Desert',
+                'Water',
+                'Swamp',
+                'Forest',
+                'Garden')
+
 
 class Starmap:
     def __init__(self, *, filename=None, n=100, minimum_distance=5):
         if filename:
             self.data = pd.read_csv(filename)
-            self.data['ID'] = [int(i) for i in self.data['ID']]
+            # Some columns have to be converted from strings to tuples
             self.data['Coordinates'] = [eval(c) for c in self.data['Coordinates']]
-            self.data['Links'] = [eval(c) for c in self.data['Links']]
-            print(self.data['Coordinates'])
+            self.data['Whiplines'] = [eval(c) for c in self.data['Whiplines']]
         else:
             self.data = pd.DataFrame(range(n), columns=['ID'])
             self.data['Name'] = [str(i) for i in self.data['ID']]
             self.__generate_coordinates(n, minimum_distance)
             self.__generate_links()
+            self.__generate_techs()
             # self.data.set_index('ID', inplace=True)
-            # print(self.data)
+            print(self.data)
 
     def __generate_coordinates(self, n, minimum_distance):
         def f():
@@ -39,7 +59,11 @@ class Starmap:
 
         def links(a):
             return tuple(i for i, b in zip(d['ID'], d['Coordinates']) if not any_intervening_stars(a, b))
-        d['Links'] = [links(a) for a in d['Coordinates']]
+        d['Whiplines'] = [links(a) for a in d['Coordinates']]
+
+    def __generate_techs(self):
+        d = self.data
+        d['Tech'] = [TECH_LEVELS[roll_df(4) + 4] for _ in d['ID']]
 
     def plot(self, filename):
         d = self.data
@@ -48,7 +72,7 @@ class Starmap:
         plt.ylim([-5, 105])
         plt.grid()
         for a in d['ID']:
-            for b in d.iloc[a]['Links']:
+            for b in d.iloc[a]['Whiplines']:
                 plt.plot(*zip(d.iloc[a]['Coordinates'], d.iloc[b]['Coordinates']), 'r')
         for n, c in zip(d['Name'], d['Coordinates']):
             plt.annotate(n, c, textcoords='offset points', xytext=(0, 5), ha='center', size='4', zorder=3)
@@ -60,39 +84,6 @@ class Starmap:
         print('Saving to ' + filename)
         self.data.to_csv(filename)
 
-# def generate_starmap(n):
-#     stars = generate_stars(n)
-#     links = generate_links(stars)
-#     return stars, links
-#
-#
-
-#
-#
-# TECH_LEVELS = ('Nigh magical', 'Advanced whipdrive', 'Primitive whipdrive', 'Sublight', 'Orbital', 'Industrial',
-#                'Medieval', 'Iron age', 'Stone age')
-# ENVIRONS = ('Garden', 'Forest', 'Swamp', 'Water', 'Desert', 'Ice', 'Volcano', 'Vacuum', 'Asteroid')
-#
-#
-# # This all needs to updated to use a dataframe
-# def print_starmap(stars, names, links, techs, environs):
-#     print('Index,Name,X,Y,Whiplines,Tech,Environment')
-#     for i in range(len(stars)):
-#         print(f'{i},"{i}",{stars[i][0]},{stars[i][1]},"{links[i]}",{techs[i]},{environs[i]}')
-#
-#
-# def read_starmap(file):
-#     import csv
-#     with open(file) as f:
-#         reader = tuple(csv.DictReader(f))
-#     stars = tuple((int(r['X']), int(r['Y'])) for r in reader)
-#     names = tuple(r['Name'] for r in reader)
-#     links = {i: eval(r['Whiplines']) for i, r in enumerate(reader)}
-#     techs = tuple(r['Tech'] for r in reader)
-#     environs = tuple(r['Environment'] for r in reader)
-#     print(stars)
-#     print(links)
-#     return stars, names, links, techs, environs
 #
 #
 # def main():
@@ -106,7 +97,8 @@ class Starmap:
 
 
 if __name__ == '__main__':
-    m = Starmap(filename='starmap.csv')
+    m = Starmap(n=100)
+    # m = Starmap(filename='starmap.csv')
     m.plot('starmap.png')
     # m.save('starmap.csv')
 
